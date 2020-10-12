b8e79195cf09e5c9599b265a04bf9d82a815ccbd
MyeongchanJin <jmcchan@naver.com>
2020-10-12 23:29:00 +0900

    엔티티 등록 - 쓰기 지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,66 +17,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            /*
-            // 멤버 추가
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
-            em.persist(member);
-             */
 
-            /*
-            // 멤버 찾기
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
-             */
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            /*
-            // 멤버 삭제(위의 멤버 찾기에서 찾은 멤버를 파라미터로 전달)
-            Member findMember = em.find(Member.class, 1L);
-            em.remove(findMember);
-             */
+            em.persist(member1);
+            em.persist(member2);
 
-            /*
-            // 멤버 수정(persist() 필요없음. 컬렉션으로 관리하는 것 처럼 설계됨)
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
-             */
-
-            /*
-            // JPQL (SELECT)
-            List<Member> result= em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(0).setMaxResults(5)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
-             */
-
-            /*
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
-
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
-             */
-
-            // 2번 조회
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
-
-            System.out.println("result: " + (findMember1 == findMember2));
+            System.out.println("======================");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -8,11 +10,15 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor // JPA는 기본적으로 내부적으로 리플렉션을 쓰기때문에 기본 생성자를 생성해줘야 한다.
 @Setter
 public class Member {
 
     @Id
     private Long id;
     private String name;
+
+
 }
 
