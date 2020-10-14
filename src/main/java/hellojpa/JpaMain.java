8359df6fac7303010b67dbabbabbf62a59073972
MyeongchanJin <jmcchan@naver.com>
2020-10-14 23:38:02 +0900

    초기화로 준영속 상태 만들기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -22,8 +21,14 @@ public class JpaMain {
             member.setName("ZZZZZ");
 
             // em.persist(member); // 컬렉션을 다루는 것 처럼 작업하게 되므로 반영하는 코드가 필요없다.\
+
+            // 준영속 상태 만들기 1.
             // 영속성 컨텍스트에서 빼내기
             em.detach(member);
+
+            // 준영속 상태 만들기 2.
+            // 영속성 컨텍스트 초기화
+            em.clear();
 
             System.out.println("======================");
 
