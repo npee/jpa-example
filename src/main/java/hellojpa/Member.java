7f521d44c805eeb9cbe4fd156691feceba1def08
MyeongchanJin <jmcchan@naver.com>
2020-10-22 23:13:19 +0900

    hellojpa - 일대다 양방향 매핑 예제

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -16,6 +16,10 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
     public void setUsername(String username) {
         this.username = username;
     }
