0e3875a0cf0f88152b78de1499ff9c81319db1ae
MyeongchanJin <jmcchan@naver.com>
2020-10-23 22:30:58 +0900

    hellojpa - 다대다 양방향 매핑 예제

--- src/main/java/hellojpa/Product.java
+++ src/main/java/hellojpa/Product.java
@@ -3,9 +3,9 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,4 +16,7 @@ public class Product {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
 }
