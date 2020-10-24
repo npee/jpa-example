41fc734fbb9b202e533c48812d28870ed5430f96
MyeongchanJin <jmcchan@naver.com>
2020-10-24 18:30:43 +0900

    hellojpa - 상속관계 매핑 (단일 테이블 전략)

---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,14 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private String price;
+}
