c9c90e2d75bdf98c8b6373a4912398f752476e41
npee <jmc4664@gmail.com>
2020-11-05 00:08:03 +0900

    hellojpa - 즉시 로딩의 문제점 2 (N + 1)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -23,11 +23,25 @@ public class JpaMain {
             team.setName("teamA");
             em.persist(team);
 
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
+            Team team3 = new Team();
+            team3.setName("teamC");
+            em.persist(team3);
+
             Member member1 = new Member();
             member1.setUsername("A");
             member1.setEmail("A EMAIL");
             member1.setTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("B");
+            member2.setEmail("B EMAIL");
+            member2.setTeam(team2);
+            em.persist(member2);
 
             em.flush();
             em.clear();
