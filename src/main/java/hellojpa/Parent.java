33a70437f1b629f57f6f302b8f1ef559e162ae70
npee <jmc4664@gmail.com>
2020-11-07 17:06:26 +0900

    hellojpa - 영속성 전이 테스트 cascade

--- src/main/java/hellojpa/Parent.java
+++ src/main/java/hellojpa/Parent.java
@@ -3,10 +3,7 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +16,7 @@ public class Parent {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "parent")
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child) {
