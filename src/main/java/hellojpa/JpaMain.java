3970f6bc64ebd10dd15673a478c991fc97d93f64
npee <jmc4664@gmail.com>
2020-11-08 21:24:57 +0900

    hellojpa - 값 타입 컬렉션 수정(List<Address>)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -40,11 +40,9 @@ public class JpaMain {
 
             System.out.println("+++++++++++++ START ++++++++++++++");
             Member findMember = em.find(Member.class, member.getId());
-            Address oldAddress = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
 
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
+            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
