66b83aef16776b22864f9c425eba924b052902f4
npee <jmc4664@gmail.com>
2020-11-08 20:10:58 +0900

    hellojpa - 값 타입 컬렉션 조회(Member만)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -32,6 +33,12 @@ public class JpaMain {
             member.getAddressHistory().add(new Address("old2", "street", "10000"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("+++++++++++++ START ++++++++++++++");
+            Member findMember = em.find(Member.class, member.getId());
 
             tx.commit();
         } catch (Exception e) {
