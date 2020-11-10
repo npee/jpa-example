d23be4a8d17a272eecfebf18f4ebe7826a0fd887
npee <jmc4664@gmail.com>
2020-11-10 23:20:22 +0900

    jpql - 패키지 생성, 초기화

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,46 @@
+package jpql;
+
+import hellojpa.embedded.Address;
+import hellojpa.embedded.AddressEntity;
+import hellojpa.embedded.Period;
+import org.hibernate.Hibernate;
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Set;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            em.flush();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
