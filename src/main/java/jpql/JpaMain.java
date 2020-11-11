467c3fde4c20e240a4af6b1d1cb748f3b8737812
npee <jmc4664@gmail.com>
2020-11-11 23:58:20 +0900

    jpql - TypedQuery, Query

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,20 +1,6 @@
 package jpql;
 
-import hellojpa.embedded.Address;
-import hellojpa.embedded.AddressEntity;
-import hellojpa.embedded.Period;
-import org.hibernate.Hibernate;
-import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
-import java.util.Set;
+import javax.persistence.*;
 
 public class JpaMain {
 
@@ -29,7 +15,15 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            // 반환 타입이 명확할 때
+            TypedQuery<Member> query1 = em.createQuery("select m from Member as m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member as m", String.class);
+
+            // 반환 타입이 명확하지 않을 때
+            Query query3 = em.createQuery("select m.username, m.age from Member as m");
 
             em.flush();
 
