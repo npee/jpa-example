c6faca4cdcab46734fa1280aa0aee12913b58b1e
npee <jmc4664@gmail.com>
2020-11-13 01:02:49 +0900

    jpql - 파라미터 바인딩

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,30 +19,11 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            // 반환 타입이 명확할 때
-            TypedQuery<Member> query1 = em.createQuery("select m from Member as m", Member.class);
-            TypedQuery<Member> query2 = em.createQuery("select m from Member m where m.id = 1", Member.class);
-            TypedQuery<Team> query3 = em.createQuery("select t from Team t", Team.class);
-            TypedQuery<Team> query4 = em.createQuery("select t from Team t where t.id = 1", Team.class);
-
-            // 결과가 하나 이상일 때
-            List<Member> resultList = query1.getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1.getUsername());
-            }
-
-            List<Team> teamList = query3.getResultList();
-            System.out.println("teamList size = " + teamList.size());
-            for (Team team : teamList) {
-                System.out.println("team = " + team.getName());
-            }
-
-            // 결과가 딱 하나일 때
-            Member singleResult = query2.getSingleResult();
-            System.out.println("singleResult = " + singleResult);
-
-            Team team = query4.getSingleResult();
-            System.out.println("team.getName() = " + team.getName());
+            TypedQuery<Member> query1 =
+                    em.createQuery("select m from Member as m where m.username = :username", Member.class);
+            query1.setParameter("username" ,"member1");
+            Member singleResult = query1.getSingleResult();
+            System.out.println("singleResult = " + singleResult.getUsername());
 
             em.flush();
 
