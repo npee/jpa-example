dbb8f9002eb3a090b380d6f00d4ff74db2de6bc2
npee <jmc4664@gmail.com>
2020-11-12 23:54:37 +0900

    jpql - getListResult(), getSingleResult()

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -20,10 +21,18 @@ public class JpaMain {
 
             // 반환 타입이 명확할 때
             TypedQuery<Member> query1 = em.createQuery("select m from Member as m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member as m", String.class);
+            TypedQuery<Member> query2 = em.createQuery("select m from Member m where m.id = 1", Member.class);
 
-            // 반환 타입이 명확하지 않을 때
-            Query query3 = em.createQuery("select m.username, m.age from Member as m");
+            // 결과가 하나 이상일 때
+            List<Member> resultList = query1.getResultList();
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            // 결과가 딱 하나일 때
+            Member singleResult = query2.getSingleResult();
+            System.out.println("singleResult = " + singleResult);
+
 
             em.flush();
 
