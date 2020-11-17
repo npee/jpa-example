71e47c0f59e65818fddc9014f65a44695a22a66f
npee <jmc4664@gmail.com>
2020-11-17 23:15:40 +0900

    jpql - jpql 함수 - 문자열 합치기 (||)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,31 +15,19 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("team1");
-            em.persist(team);
-
             Member member1 = new Member();
             member1.setUsername("member1");
-            member1.setAge(10);
-            member1.setMemberType(MemberType.USER);
-
-            member1.setTeam(team);
-
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자");
-            member2.setAge(20);
-
-            member2.setTeam(team);
-
+            member1.setUsername("member2");
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select nullif(m.username, '관리자') as username from Member m";
+            String query = "select 'a' || 'b' from Member m";
+
             List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
 
