258106ee0062ea61651207affe384ec73dc4def8
npee <jmc4664@gmail.com>
2020-11-23 00:53:36 +0900

    jpql - 컬렉션 조인 페이징 - 2. @BatchSize 설정

--- src/main/java/jpql/Team.java
+++ src/main/java/jpql/Team.java
@@ -2,6 +2,7 @@ package jpql;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class Team {
     private Long id;
     private String name;
 
+    @BatchSize(size = 100)
     @OneToMany(mappedBy ="team")
     private List<Member> members = new ArrayList<>();
 
