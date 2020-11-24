77fc65523cec253b1826bfc7467124c8cae2505d
npee <jmc4664@gmail.com>
2020-11-25 01:30:45 +0900

    jpql - Named Query - 1. @NamedQuery 사용

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -45,13 +45,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m where m.team.id = :teamId ";
-
-            List<Member> members = em.createQuery(query, Member.class)
-                    .setParameter("teamId", team1.getId())
+            List<Member> result = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "회원1")
                     .getResultList();
 
-            for (Member member : members) {
+            for (Member member : result) {
                 System.out.println("member = " + member);
             }
 
